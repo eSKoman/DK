2de693f4298abf588a24e50959cddb5c78bd9426
Slavomír Kotlár <57029442+eSKoman@users.noreply.github.com>
2022-07-15 18:17:28 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,18 +1,35 @@
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-  
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
   }
-  
-  window.addEventListener("scroll", reveal);
-  
+}
+
+window.addEventListener("scroll", reveal);
+
+
+var images = [
+  "../media/praca/1.jpg",
+  "../media/praca/2.jpg",
+  "../media/praca/3.jpg"
+]
+
+var imageHead = document.querySelector(".showcase-area");
+
+var i = 0;
+setInterval(function () {
+  imageHead.style.backgroundImage = "url(" + images[i] + ")";
+  i = i + 1;
+  if (i == images.length) {
+    i = 0;
+  }
+}, 3000);
