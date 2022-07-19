1e665cc48c39c190da1c75f45dce42591f3c9031
Slavomír Kotlár <57029442+eSKoman@users.noreply.github.com>
2022-07-19 17:04:50 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -27,7 +27,9 @@ var imageHead = document.querySelector(".showcase-area");
 
 var i = 0;
 setInterval(function () {
+
   imageHead.style.backgroundImage = "url(" + images[i] + ")";
+
   i = i + 1;
   if (i == images.length) {
     i = 0;
